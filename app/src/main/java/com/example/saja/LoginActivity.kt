cef659ad848d6f1a5d58432c94f7b3b27b137f03
xpereez <777elalexito@gmail.com>
2022-01-12 22:47:37 +0100

    REGISTRO(FIRESTORE Y AUTENTICACION) E INICIO DE SESION EMAIL/PASSWORD FUNCIONANDO CORRECTAMENTE

--- app/src/main/java/com/example/saja/LoginActivity.kt
+++ app/src/main/java/com/example/saja/LoginActivity.kt
@@ -1,11 +1,68 @@
 package com.example.saja
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+    }
+
+    fun onClickLoginn(view: View){
+        //Obtener texto de email y pass box
+        val emailllogin : EditText = findViewById(R.id.correoLogin)
+        val passslogin : EditText = findViewById(R.id.passLogin)
+
+        //Ver que no nulos
+        if(emailllogin.text.isNotEmpty() && passslogin.text.isNotEmpty()){
+            var emailLoginString = emailllogin.text.toString()
+            var passLoginString = passslogin.text.toString()
+
+            try {
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailLoginString,passLoginString)
+                    .addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val main= Intent(this, MainActivity::class.java)
+                            startActivity(main)
+
+                        }
+                        else{
+                            showAlert()
+                        }
+                    }
+
+            }catch (ex: Exception){
+
+                showAlert()
+            }
+        }else{
+            showAlertCamposVacios()
+        }
+    }
+
+    fun showAlertCamposVacios() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("ERROR")
+        builder.setMessage("Los campos no pueden estar vacíos")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("ERROR")
+        builder.setMessage("Se ha producido un error on firebase")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/saja/RegistroActivity.kt
+++ app/src/main/java/com/example/saja/RegistroActivity.kt
@@ -19,30 +19,34 @@ class RegistroActivity : AppCompatActivity() {
     fun onClickdb(view: android.view.View){
         val correo : EditText = findViewById(R.id.email)
         val password : EditText = findViewById(R.id.password)
+        val password1 : EditText = findViewById(R.id.password1)
+        val username : EditText = findViewById(R.id.UsernameText)
 
 
-        if (password.text.toString()!= "" && correo.text.toString() != "") {
+            if (password.text.toString()!= "" && correo.text.toString() != "") {
+                        if(password.text.toString()==password1.text.toString()){
+                            db.collection("users").document(username.text.toString()).set(hashMapOf("email" to correo.text.toString(),"password" to password.text.toString()))
+                            FirebaseAuth.getInstance()
+                                .createUserWithEmailAndPassword(correo.text.toString(), password.text.toString())
+                                .addOnCompleteListener {
+                                    if (it.isSuccessful) {
+                                        db.collection("users").document(username.text.toString()).set(hashMapOf("email" to correo.text.toString(),"password" to password.text.toString()))
+                                        val main= Intent(this, MainActivity::class.java)
+                                        startActivity(main)
+                                    } else {
+                                        //Error
+                                        showAlert()
+                                    }
+                                }
+                        }else {
+                            //Error
+                            showAlertPasswordIguales()
+                        }
 
-            db.collection("users").document(correo.text.toString()).set(hashMapOf("password" to password.text.toString()))
-
-            FirebaseAuth.getInstance()
-                .createUserWithEmailAndPassword(correo.text.toString(), password.text.toString())
-                .addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        db.collection("users").document(correo.text.toString()).set(hashMapOf("password" to password.text.toString()))
-                        val main= Intent(this, MainActivity::class.java)
-                        startActivity(main)
-                    } else {
-                        //Error
-                        showAlert()
-                    }
-
-                }
-
-        }else {
-            //Error
-            showAlert()
-        }
+            }else {
+                //Error
+                showAlertCamposVacios()
+            }
 
     }
     fun onClickInicio(view: View){
@@ -53,10 +57,26 @@ class RegistroActivity : AppCompatActivity() {
         val pantallaInicio = Intent(this, InicioActivity::class.java)
         startActivity(pantallaInicio)
     }
+    fun showAlertCamposVacios() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("ERROR")
+        builder.setMessage("Los campos no pueden estar vacíos")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    fun showAlertPasswordIguales() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("ERROR")
+        builder.setMessage("Las contraseñas no coinciden")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
     fun showAlert() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("ERROR")
-        builder.setMessage("Se ha producido un error con firebase")
+        builder.setMessage("Se ha producido un error on firebase")
         builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
