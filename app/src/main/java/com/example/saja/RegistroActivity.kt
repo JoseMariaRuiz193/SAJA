74a84a0040e8f5424b1fc51c56aba0c472172bd3
xpereez <777elalexito@gmail.com>
2022-01-12 20:21:06 +0100

    build gradle error

--- app/src/main/java/com/example/saja/RegistroActivity.kt
+++ app/src/main/java/com/example/saja/RegistroActivity.kt
@@ -4,11 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class RegistroActivity : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registro)
+    }
+    fun onClickdb(view: android.view.View){
+        val correo : EditText = findViewById(R.id.email)
+        val password : EditText = findViewById(R.id.password)
+
+
+        if (password.text.toString()!= "" && correo.text.toString() != "") {
+
+            db.collection("users").document(correo.text.toString()).set(hashMapOf("password" to password.text.toString()))
+
+            FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(correo.text.toString(), password.text.toString())
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        db.collection("users").document(correo.text.toString()).set(hashMapOf("password" to password.text.toString()))
+                        val main= Intent(this, MainActivity::class.java)
+                        startActivity(main)
+                    } else {
+                        //Error
+                        showAlert()
+                    }
+
+                }
+
+        }else {
+            //Error
+            showAlert()
+        }
+
     }
     fun onClickInicio(view: View){
         irPantallaInicio()
@@ -17,5 +52,13 @@ class RegistroActivity : AppCompatActivity() {
     fun irPantallaInicio() {
         val pantallaInicio = Intent(this, InicioActivity::class.java)
         startActivity(pantallaInicio)
+    }
+    fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("ERROR")
+        builder.setMessage("Se ha producido un error con firebase")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
