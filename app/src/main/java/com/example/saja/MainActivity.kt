d1ea5989da110443cb7b156fa02f0f0b972da3fe
hdezgonia <hdezgonia@gmail.com>
2022-02-01 20:25:27 +0100

    API + Políticas + Cambios menores

--- app/src/main/java/com/example/saja/MainActivity.kt
+++ app/src/main/java/com/example/saja/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.saja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.widget.TextView
+import io.finnhub.api.apis.DefaultApi
+import io.finnhub.api.infrastructure.ApiClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        val apiClient = DefaultApi()
+        ApiClient.apiKey["token"] = "sandbox_c7jgp8iad3i887nsdhgg"
+        val json = apiClient.quote("TSLA")
+        val currentprice = json.c
+
+        val tv: TextView = findViewById(R.id.price)
+        tv.setText(currentprice.toString())
+
+
     }
 }
