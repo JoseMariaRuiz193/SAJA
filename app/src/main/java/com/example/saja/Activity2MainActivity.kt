ac9e4d26fabeb8fcba1d8f8e5e3dcba84b3c5cca
sergiovillodre <sergiovillodreestrada@gmail.com>
2022-02-10 18:03:12 +0100

    nueva pantalla Main2

---
+++ app/src/main/java/com/example/saja/Activity2MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.saja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity2_main)
+    }
+}
